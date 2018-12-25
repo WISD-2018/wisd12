589507692d464ec129791754804fe1fe83f51aec
3a532043 <s3a532043@student.ncut.edu.tw>
2018-12-25 12:49:01 +0800

    bb.b2

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/bb/b1', function () {
     return view('bb.b1');
 });
+Route::get('/bb/b2', function () {
+    return view('bb.b2');
+});
